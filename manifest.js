4b35e5592de231fe148f7d3ca46a95e6db325b1f
William G Wong <wgwong@mit.edu>
2015-04-11 14:25:12 -0400

    a

--- manifest.js
+++ manifest.js
@@ -1,9 +1,9 @@
 {
     "name": "hackdartmouth_chrome_extension",
     "version": "0.0.1",
-    "manifest_version": 1,
+    "manifest_version": 2,
     "browser_action": {
-        "default_title": "That's the tool tip",
+        "default_title": "Lock your screen",
         "default_popup": "popup.html"
     }
 }
