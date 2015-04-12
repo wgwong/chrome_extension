a8e4dcba6fa99cf8ce9bea664166f96b6b6cb7c2
William G Wong <wgwong@mit.edu>
2015-04-11 22:39:11 -0400

    stores in localstorage

--- popup.js
+++ popup.js
@@ -19,16 +19,39 @@ function expandFullScreen()
     });
 }
 
+$(document).ready(function() {
+  $("#button1").click( function() {
+    var frontside = $("#front").val();
+    var backside = $("#back").val();
+    var temp = {"front": frontside, "back": backside};
+    var temp_str = JSON.stringify(temp);
+    localStorage.setItem("result", temp_str);
+    var temp2 = localStorage.getItem("result");
+    var temp3 = JSON.parse(temp2);
+    $("#f").replaceWith(temp3.front);
+    $("#b").replaceWith(temp3.back);
+    console.log(temp2);
+    console.log(temp3);
+    alert(temp2); //debug
+    alert(temp3); //debug
+  });
+  var temp2 = localStorage.getItem("result");
+  var temp3 = JSON.parse(temp2);
+  $("#f").replaceWith(temp3.front);
+  $("#b").replaceWith(temp3.back);
+});
 
+/*
 document.getElementById('button1').addEventListener('click', function(){
   var frontside = document.getElementById('front').value;
   var backside = document.getElementById('back').value;
   var temp = {front: frontside, back: backside};
   localStorage.setItem("result", temp);
   var temp2 = localStorage.getItem("result");
-  document.getElementById('f').innerHTML = temp2.back;
+  document.getElementById('f').innerHTML = "arbitrarystring"; //debug
+  //document.getElementById('f').innerHTML = temp2.back;
 });
 
   function partb() {
 
-  }
+  }*/
